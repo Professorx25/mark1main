f79cc0b6b3dadae4cb4957ed4cf0997f520c10c4
Vaibhav0-07 <vaibhavpandey2506@gmail.com>
2026-03-09 23:30:05 +0530

    03 Authentication

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-screen items-center justify-center">
       <Button>Test</Button>
+      <UserButton/>
     </div>
   );
 }
